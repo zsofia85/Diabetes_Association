b57617af66b7b744a6ce205ca439191f09c1570e
Alexandru-Florin Bogdan <bogdan.alexandru1311@gmail.com>
2020-02-26 00:12:56 +0100

    Add subcategories page. Add css for subcategories

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,14 +6,12 @@ const productCategories = [
             {
                 name: 'Dressing and sauces',
                 image: '/subcategories/dressings.jpg',
-                limits: [
-                    {
-                        description: 'Suggested values per 100g',
-                        sugar: null,
-                        fat: '5g',
-                        fibre: null
-                    }
-                ]
+                limits:  {
+                    description: 'Suggested values per 100g',
+                    sugar: null,
+                    fat: '5g',
+                    fibre: null
+                }
             }
         ]
     },
@@ -24,26 +22,22 @@ const productCategories = [
             {
                 name: 'Ryebread and crisp bread',
                 image: '/subcategories/ryebread.jpg',
-                limits: [
-                    {
-                        description: 'Suggested values per 100g',
-                        sugar: 'max 5g',
-                        fat: 'max 7g',
-                        fibre: 'min 5g'
-                    }
-                ]
+                limits: {
+                    description: 'Suggested values per 100g',
+                    sugar: 'max 5g',
+                    fat: 'max 7g',
+                    fibre: 'min 5g'
+                }
             },
             {
                 name: 'Wholegrain bread and buns',
                 image: '/subcategories/whole-grain.jpg',
-                limits: [
-                    {
-                        description: 'Suggested values per 100g',
-                        sugar: 'max 5g',
-                        fat: 'max 7g',
-                        fibre: 'min 5g'
-                    }
-                ]
+                limits: {
+                    description: 'Suggested values per 100g',
+                    sugar: 'max 5g',
+                    fat: 'max 7g',
+                    fibre: 'min 5g'
+                }
             }
         ]
     },
@@ -54,50 +48,43 @@ const productCategories = [
             {
                 name: 'Milk',
                 image: '/subcategories/milk.jpg',
-                limits: [
-                    {
-                        description: 'Suggested values',
-                        sugar: null,
-                        fat: '0,7%',
-                        fibre: null
-                    }
-                ]
+                limits: {
+                    description: 'Suggested values',
+                    sugar: null,
+                    fat: '0,7%',
+                    fibre: null
+                }
+                
             },
             {
                 name: 'Cheese',
                 image: '/subcategories/cheese.jpg',
-                limits: [
-                    {
-                        description: 'Suggested values',
-                        sugar: null,
-                        fat: 'max 17% (30+)',
-                        fibre: null
-                    }
-                ]
+                limits: {
+                    description: 'Suggested values',
+                    sugar: null,
+                    fat: 'max 17% (30+)',
+                    fibre: null
+                }
             },
             {
                 name: 'Soured dairy products',
                 image: '/subcategories/soured-milk.jpg',
-                limits: [
-                    {
-                        description: 'Suggested values',
-                        sugar: 'max 4% added sugar OR max 8g/100g',
-                        fat: 'max 1,5%',
-                        fibre: null
-                    }
-                ]
+                limits: {
+                    description: 'Suggested values',
+                    sugar: 'max 4% added sugar OR max 8g/100g',
+                    fat: 'max 1,5%',
+                    fibre: null
+                }
             },
             {
                 name: 'Chocolate milk and milk drinks',
                 image: '/subcategories/chocolate-drink.jpg',
-                limits: [
-                    {
-                        description: 'Suggested values',
-                        sugar: 'max 4% added sugar OR max  8g/100ml',
-                        fat: 'max 0,7%',
-                        fibre: null
-                    }
-                ]
+                limits: {
+                    description: 'Suggested values',
+                    sugar: 'max 4% added sugar OR max  8g/100ml',
+                    fat: 'max 0,7%',
+                    fibre: null
+                }
             }
         ]
     },
@@ -108,80 +95,68 @@ const productCategories = [
             {
                 name: 'Juices',
                 image: '/subcategories/juices.jpg',
-                limits: [
-                    {
-                        description: 'Suggested values',
-                        sugar: 'max 10g per 100ml',
-                        fat: null,
-                        fibre: null
-                    }
-                ]
+                limits: {
+                    description: 'Suggested values',
+                    sugar: 'max 10g per 100ml',
+                    fat: null,
+                    fibre: null
+                }
             },
             {
                 name: 'Soda and soft drinks',
                 image: '/subcategories/soda-and-soft-drink.jpg',
-                limits: [
-                    {
-                        description: 'Suggested values',
-                        sugar: 'max 1g per 100ml',
-                        fat: null,
-                        fibre: null
-                    }
-                ]
+                limits: {
+                    description: 'Suggested values',
+                    sugar: 'max 1g per 100ml',
+                    fat: null,
+                    fibre: null
+                }
             },
             {
                 name: 'Chocolate milk and milk drinks',
                 image: '/subcategories/chocolate-drink',
-                limits: [
-                    {
-                        description: 'Suggested values',
-                        sugar: 'max 4% added sugar OR max  8g/100ml',
-                        fat: 'max 0,7%',
-                        fibre: null
-                    }
-                ]
+                limits: {
+                    description: 'Suggested values',
+                    sugar: 'max 4% added sugar OR max  8g/100ml',
+                    fat: 'max 0,7%',
+                    fibre: null
+                }
             }
         ]
     },
     {
         name: 'Meat',
-        image: '/categories/bread.svg',
+        image: '/categories/meat.svg',
         products: [
             {
                 name: 'Meat, poultry and plantbased products',
                 image: '/subcategories/meat.jpg',
-                limits: [
-                    {
-                        description: 'Suggested values',
-                        sugar: null,
-                        fat: 'max 10%',
-                        fibre: null
-                    }
-                ]
+                limits: {
+                    description: 'Suggested values',
+                    sugar: null,
+                    fat: 'max 10%',
+                    fibre: null
+                }
             },
             {
                 name: 'Fish and sea food',
                 image: '/subcategories/fish.jpg',
-                limits: [
-                    {
-                        description: 'Suggested values: fresh, frozen, preserved or fish toppings',
-                        sugar: null,
-                        fat: null,
-                        fibre: null
-                    }
-                ]
+                limits: {
+                    description: 'Suggested values: fresh, frozen, preserved or fish toppings',
+                    sugar: null,
+                    fat: null,
+                    fibre: null
+                }
             },
             {
                 name: 'Toppings',
                 image: '/subcategories/toppings.jpg',
-                limits: [
-                    {
-                        description: 'Suggested values',
-                        sugar: null,
-                        fat: 'max 10%',
-                        fibre: null
-                    }
-                ]
+                limits: {
+                    description: 'Suggested values',
+                    sugar: null,
+                    fat: 'max 10%',
+                    fibre: null
+                }
             }
         ]
     },
@@ -192,14 +167,12 @@ const productCategories = [
             {
                 name: 'Muesli and porage',
                 image: '/subcategories/muesli.jpg',
-                limits: [
-                    {
-                        description: 'Suggested values per 100g',
-                        sugar: 'max 13g',
-                        fat: 'max 8g',
-                        fibre: 'min 6g'
-                    }
-                ]
+                limits:  {
+                    description: 'Suggested values per 100g',
+                    sugar: 'max 13g',
+                    fat: 'max 8g',
+                    fibre: 'min 6g'
+                }
             }
         ]
     },
@@ -210,25 +183,22 @@ const productCategories = [
             {
                 name: 'Potatoes and potato products',
                 image: '/subcategories/potatoes.jpg',
-                limits: [
-                    {
-                        description: 'Suggested values per 100g',
-                        sugar: null,
-                        fat: 'max 3g',
-                        fibre: null
-                    }
-                ]
+                limits: {
+                    description: 'Suggested values per 100g',
+                    sugar: null,
+                    fat: 'max 3g',
+                    fibre: null
+                }
             },
             {
                 name: 'Veggies and fruits',
-                limits: [
-                    {
-                        description: 'Suggested values: fresh, frozen or preserved without added sugar',
-                        sugar: null,
-                        fat: null,
-                        fibre: null
-                    }
-                ]
+                image: '/subcategories/veggs-and-fruits.jpg',
+                limits: {
+                    description: 'Suggested values: fresh, frozen or preserved without added sugar',
+                    sugar: null,
+                    fat: null,
+                    fibre: null
+                }
             }
         ]
     },
@@ -239,26 +209,22 @@ const productCategories = [
             {
                 name: 'Food to go',
                 image: '/subcategories/food-to-go.jpg',
-                limits: [
-                    {
-                        description: 'Suggested values per 100g',
-                        sugar: null,
-                        fat: 'max 5g recommended with vegetables or whole grain',
-                        fibre: null
-                    }
-                ]
+                limits: {
+                    description: 'Suggested values per 100g',
+                    sugar: null,
+                    fat: 'max 5g recommended with vegetables or whole grain',
+                    fibre: null
+                }
             },
             {
                 name: 'Ready made dinner',
                 image: '/subcategories/ready-made-dinner.jpg',
-                limits: [
-                    {
-                        description: 'Suggested values per 100g',
-                        sugar: null,
-                        fat: 'max 5g recommended with vegetables or whole grain',
-                        fibre: null
-                    }
-                ]
+                limits: {
+                    description: 'Suggested values per 100g',
+                    sugar: null,
+                    fat: 'max 5g recommended with vegetables or whole grain',
+                    fibre: null
+                }
             }
         ]
     },
@@ -271,6 +237,7 @@ const disclaimerPage = document.getElementById('disclaimer');
 const responsivePage = document.getElementById('responsive');
 const individualRecipePage = document.getElementById('individualRecipe');
 const productCategoriesPage = document.getElementById('productCategories');
+const productsSubCategoriesPage = document.getElementById('productsSubCategories');
 
 
 function displayElements(elementsToShow, elementsToHide) {
@@ -286,6 +253,11 @@ function displayElements(elementsToShow, elementsToHide) {
 function openProductCategoryPage() {
     displayElements([productCategoriesPage, navigation], [disclaimerPage, responsivePage, individualRecipePage])
     renderProductCategories(productCategories);
+}
+
+function openSubCategoryPage(productCategory) {
+    displayElements([productsSubCategoriesPage, navigation], [disclaimerPage, responsivePage, individualRecipePage, productCategoriesPage]);
+    renderProductSubCategories(productCategory);
 }
 
 function renderProductCategories(productCategories) {
@@ -306,7 +278,45 @@ function renderProductCategories(productCategories) {
         listItem.appendChild(itemImage);
         listItem.appendChild(itemText);
 
+        listItem.addEventListener('click', function() {
+            openSubCategoryPage(p);
+        });
+
         productList.appendChild(listItem);
+    }
+}
+
+function renderProductSubCategories(productCategory) {
+    console.log(productCategory);
+    let list = document.getElementById('productsSubCategoriesList');
+    let productCategoryName = document.getElementById('subCategoryName');
+    productCategoryName.innerHTML = productCategory.name
+
+    for (let p of productCategory.products) {
+        let listItem = document.createElement('li');
+
+        let subCategoryNameElem = document.createElement('h6');
+        subCategoryNameElem.innerHTML = p.name;
+        subCategoryNameElem.classList.add('subcategory-name');
+    
+        let subcategoryImageElem = document.createElement('img');
+        subcategoryImageElem.setAttribute('src', imgPath + p.image);
+        subcategoryImageElem.setAttribute('alt', p.name + ' banner');
+    
+        let subCategoryDescElem = document.createElement('p');
+        subCategoryDescElem.innerHTML = p.limits.description;
+        subCategoryDescElem.classList.add('description')
+    
+        let subCategoryLimitsElem = document.createElement('ul');
+        subCategoryLimitsElem.classList.add('limits')
+
+        listItem.appendChild(subCategoryNameElem);
+        listItem.appendChild(subcategoryImageElem);
+        listItem.appendChild(subCategoryDescElem);
+
+        // ITERATE THROUGH LIMITS
+
+        list.appendChild(listItem);
     }
 }
 
